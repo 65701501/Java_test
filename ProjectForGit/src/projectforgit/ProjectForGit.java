f7427eb8f6c30ced77031498c97c83391560e35b
damie <damie@R2-D2>
2026-02-18 11:49:16 +0200

    Java_Ap first commit

---
+++ ProjectForGit/src/projectforgit/ProjectForGit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectforgit;
+
+/**
+ *
+ * @author damie
+ */
+public class ProjectForGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("My name is John");     
+                }
+    
+}
