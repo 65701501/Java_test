c8835215f8a38442b884a76ef451380047743627
damie <damie@R2-D2>
2026-02-18 11:53:35 +0200

    Added Decimal practice calc

--- ProjectForGit/src/projectforgit/ProjectForGit.java
+++ ProjectForGit/src/projectforgit/ProjectForGit.java
@@ -14,7 +14,19 @@ public class ProjectForGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("My name is John");     
+        System.out.println("My name is John"); 
+        
+        double firstNum = 3.6e3d;     //e in float/double means 10 to the power
+double secondNum = 5.89e4d;    // end double/float numbers in d/f  
+double calcfinal;
+calcfinal = firstNum*secondNum;        //Another Real-Life example
+
+System.out.println("Decimal Calculater:");
+System.out.println();
+System.out.println("First Decimal Number: " + firstNum);
+System.out.println("Second Decimal Number: " + secondNum);
+System.out.println();
+System.out.println("Final Total Calculation: " + calcfinal);
                 }
     
 }
